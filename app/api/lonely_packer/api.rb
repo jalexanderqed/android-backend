4fad729e57538b9f65d22afbad5c85cbb333afd7
jalexanderqed <jalexander.qed@gmail.com>
2016-03-12 15:37:45 -0800

    Initial commit.

---
+++ app/api/lonely_packer/api.rb
@@ -0,0 +1,28 @@
+module LonelyPacker
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'lonely_packer'
+    format :json
+    prefix :api
+
+    helpers do
+
+    end
+
+    resource :users do
+      params do
+
+      end
+      get '/' do
+        Hash['test', 'response']
+      end
+
+      desc 'Create a new user.'
+      params do
+        requires :username, type: String
+      end
+      post 'create' do
+        Hash['test', 'response']
+      end
+    end
+  end
+end
