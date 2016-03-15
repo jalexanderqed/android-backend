d4cf1c60381bdeabf78435ab0806cbad2e5614cc
John Alexander <jalexander.qed@gmail.com>
2016-03-14 21:16:33 -0700

    Updated API paremeters.

--- app/api/lonely_packer/api.rb
+++ app/api/lonely_packer/api.rb
@@ -26,7 +26,9 @@ module LonelyPacker
         requires :birth_year, type: Integer
         requires :birth_month, type: Integer
         requires :birth_day, type: Integer
-        requires :visited_countries, type: Array(String)
+        optional :visited_countries, type: Array do
+          requires :name, type: String
+        end
         optional :bio, type: String
       end
       post '/' do
@@ -43,8 +45,11 @@ module LonelyPacker
               birth_month: params[:birth_month],
               birth_day: params[:birth_day],
               bio: params[:bio])
-          params[:visited_countries].each do |c|
-            u.visited_countries.create(name: c)
+
+          if params[:visited_countries].present?
+            params[:visited_countries].each do |c|
+              u.visited_countries.create(name: c[:name])
+            end
           end
 
           status 201
@@ -54,60 +59,22 @@ module LonelyPacker
         end
       end
 
-      desc 'Update a user.'
-      params do
-        requires :username, type: String
-        requires :password, type: String
-        optional :real_name, type: String
-        optional :birth_year, type: Integer
-        optional :birth_month, type: Integer
-        optional :birth_day, type: Integer
-        optional :visited_countries, type: Array(String)
-        optional :bio, type: String
-      end
-      put '/' do
-        begin
-          u = User.find_by(username: params[:username]);
-
-          if !u.present? || !(u.password == params[:password])
-            error! 'Invalid login attempt.', 401
-          end
-
-          u.password = params[:password] if params[:password].present?
-          u.real_name = params[:real_name] if params[:real_name].present?
-          u.birth_year = params[:birth_year] if params[:birth_year].present?
-          u.birth_month = params[:birth_month] if params[:birth_month].present?
-          u.birth_day = params[:birth_day] if params[:birth_day].present?
-          u.bio = params[:bio] if params[:bio].present?
-
-          params[:visited_countries].each do |c|
-            if !u.visited_countries.find_by(name: c).present?
-              u.visited_countries.create(name: c)
-            end
-          end
-
-          u.save!
-
-          status 200
-          present u, with: User::Entity
-        rescue
-          error! 'Unexpected error', 500
-        end
-      end
-
       route_param :username, type: String do
 
         before do
+          unless params[:username].present?
+            error! 'Invalid login attempt.', 401
+          end
+
           @user = User.find_by(username: params[:username]);
 
-          if !@user.present? || !(@user.password == params[:password])
+          unless @user.present? && headers['Authorization'].present? && headers['Authorization'] == @user.password
             error! 'Invalid login attempt.', 401
           end
         end
 
         desc 'Returns a specific user.'
         params do
-          requires :password, type: String
         end
         get '/' do
           begin
@@ -118,9 +85,43 @@ module LonelyPacker
           end
         end
 
+        desc 'Update a user.'
+        params do
+          optional :real_name, type: String
+          optional :birth_year, type: Integer
+          optional :birth_month, type: Integer
+          optional :birth_day, type: Integer
+          optional :visited_countries, type: Array do
+            requires :name, type: String
+          end
+          optional :bio, type: String
+        end
+        put '/' do
+          begin
+            @user.password = params[:password] if params[:password].present?
+            @user.real_name = params[:real_name] if params[:real_name].present?
+            @user.birth_year = params[:birth_year] if params[:birth_year].present?
+            @user.birth_month = params[:birth_month] if params[:birth_month].present?
+            @user.birth_day = params[:birth_day] if params[:birth_day].present?
+            @user.bio = params[:bio] if params[:bio].present?
+
+            params[:visited_countries].each do |c|
+              if !@user.visited_countries.find_by(name: c[:name]).present?
+                @user.visited_countries.create(name: c[:name])
+              end
+            end
+
+            @user.save!
+
+            status 200
+            present @user, with: User::Entity
+          rescue
+            error! 'Unexpected error', 500
+          end
+        end
+
         desc 'Adds a check in for a user.'
         params do
-          requires :password, type: String
           requires :latitude, type: Float
           requires :longitude, type: Float
           optional :comment, type: String
